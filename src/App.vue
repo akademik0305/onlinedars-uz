b504643c950575dce9a179ed53bda232c43627a8
akademik <tuychiyevakademik@gmail.com>
2023-05-23 17:11:35 +0500

    tingpng

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<script setup>
-import HeaderComponent from '@/components/HeaderComponent.vue'
-import RecommendedComponent from '@/components/RecommendedComponent.vue'
-import ChooseComponent from '@/components/ChooseComponent.vue'
-import AboutCourse from '@/components/AboutCourse.vue'
-import CharacteristicComponent from '@/components/CharacteristicComponent.vue'
-import KnowledgeComponent from '@/components/KnowledgeComponent.vue'
-import OrganizedComponent from '@/components/OrganizedComponent.vue'
-import CompleteComponent from '@/components/CompleteComponent.vue'
-import CommentsComponent from '@/components/CommentsComponent.vue'
-import PaymentComponent from '@/components/PaymentComponent.vue'
-import QuestionsComponent from '@/components/QuestionsComponent.vue'
-import FooterComponent from '@/components/FooterComponent.vue'
-
-components: {
-  HeaderComponent,
-  RecommendedComponent,
-  ChooseComponent,
-  AboutCourse,
-  CharacteristicComponent,
-  KnowledgeComponent,
-  OrganizedComponent,
-  CompleteComponent,
-  CommentsComponent,
-  PaymentComponent,
-  QuestionsComponent,
-  FooterComponent
-}
-
-</script >
-
 <template>
-  <header-component></header-component>
-  <recommended-component></recommended-component>
-  <choose-component></choose-component>
-  <about-course></about-course>
-  <characteristic-component></characteristic-component>
-  <knowledge-component></knowledge-component>
-  <organized-component></organized-component>
-  <complete-component></complete-component>
-  <comments-component></comments-component>
-  <payment-component></payment-component>
-  <questions-component></questions-component>
-  <footer-component></footer-component>
+  <div>
+    <main-page></main-page>
+    <!-- <dashboard></dashboard> -->
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import MainPage from '@/components/MainPage.vue'
+// import Dashboard from '@/dashboard/Dashboard.vue'
+components: {
+  // Dashboard
+  MainPage
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,30 @@
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
 // smooth scrool
 import VueSmoothScroll from 'vue3-smooth-scroll'
+// import  bootstrap  from 'bootstrap/dist/js/bootstrap.bundle'
 
- 
+//Dashboard
+// import '@/assets/dashboard/icon/themify-icons/themify-icons.css' 
+// import '@/assets/dashboard/icon/feather/css/feather.css' 
+// import '@/assets/dashboard/pages/menu-search/css/component.css' 
+// import '@/assets/dashboard/css/style.css' 
+// import '@/assets/dashboard/css/jquery.mCustomScrollbar.css' 
 
+// import '@/assets/dashboard/bower_components/bootstrap/dist/js/bootstrap.min.js'
+
+// import 'bootstrap/dist/css/bootstrap.css'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import './assets/sass/style.sass'
 
 
+// Vue.use(IconsPlugin)
+// Vue.use(BootstrapVue)
 
 createApp(App).use(VueSmoothScroll, {
   duration: 800,
   offset: -150
-}).mount('#app')
+})
+.mount('#app')
